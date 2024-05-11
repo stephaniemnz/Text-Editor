5303878cd25b489ba1ceac67cfc6c75cc6d7c699
stephramos <stephaniemnz@gmail.com>
2024-05-11 14:49:15 -0500

    completed TODOs

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,26 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  butInstall.style.display = "block";
+});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+//Implement a click event handler on the `butInstall` element
+butInstall.addEventListener("click", async () => {
+  butInstall.style.display = "none";
+  deferredPrompt.prompt();
+  const choiceResult = await deferredPrompt.userChoice;
+  if (choiceResult.outcome === "accepted") {
+    console.log("User accepted the install prompt");
+  } else {
+    console.log("User dismissed the install prompt");
+  }
+  deferredPrompt = null;
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+//Add an handler for the `appinstalled` event
+window.addEventListener("appinstalled", (event) => {
+  console.log("Jate PWA installed", event);
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,29 +1,58 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        chunks: ["main"],
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "service-worker.js",
+      }),
+      new WebpackPwaManifest({
+        name: "My Progressive Web App",
+        short_name: "MyPWA",
+        description: "My awesome Progressive Web App!",
+        background_color: "#ffffff",
+        crossorigin: "use-credentials",
+        icons: [
+          {
+            src: path.resolve("src/images/icon.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+          },
+        ],
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
       ],
     },
   };
